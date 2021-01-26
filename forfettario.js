d8bd1fc957910e27457fb317ee4384b483a8fe4a
MatteoRigamonti <matteo_rigamonti@hotmail.it>
2021-01-26 16:39:36 +0100

    v0.2

--- forfettario.js
+++ forfettario.js
@@ -1,18 +1,21 @@
-// v0.1
+// v0.2
 
 // Inserisci guadagno annuale
-var guadagnoAnnuale = 65000;
+let guadagnoAnnuale = 65000;
 
 // Inserisci il coefficiente
-var coefficiente = 78;
+let coefficiente = 78;
 
 // Inserisci l'imposta sostitutiva
-var impSostitut = 15;
+let impSostitut = 15;
 
 // Calcolo reddito imponibile
-var redImp = (guadagnoAnnuale * coefficiente) / 100;
+let redImp = (guadagnoAnnuale * coefficiente) / 100;
 console.log(redImp);
 
 // Imposta da pagare
-var imposta = (redImp * impSostitut) / 100;
+let imposta = (redImp * impSostitut) / 100;
 console.log("Imposta sostitutiva al " + impSostitut + "%: " + imposta + "€");
+
+document.getElementById("guadagno-annuale").textContent = "Guadagno inserito: " + guadagnoAnnuale + "€";
+document.getElementById("imposta").textContent = "Imposta da pagare: " + imposta + "€";
