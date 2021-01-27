6a461d698baa6d57948c37c242da55bce056a80a
MatteoRigamonti <matteo_rigamonti@hotmail.it>
2021-01-27 09:41:16 +0100

    v0.3

--- forfettario.js
+++ forfettario.js
@@ -1,21 +1,20 @@
-// v0.2
+// v0.3
 
-// Inserisci guadagno annuale
-let guadagnoAnnuale = 65000;
+function calcolaGuadagno() {
+	let letVarGuadagn = document.getElementById("guadagno-annuale").value; // Prendere valore nel campo "Guadagno annuale"
+	let coefficiente = document.getElementById("coefficiente-redditivita").value; // Prende valore nel campo "Coeffficiente di Redditività"
+	console.log(coefficiente);
 
-// Inserisci il coefficiente
-let coefficiente = 78;
+	let impSostitut = document.getElementById("imposta-sostitutiva").value; // Prendere valore nel campo "Imposta sostitutiva"
 
-// Inserisci l'imposta sostitutiva
-let impSostitut = 15;
+	// Calcolo reddito imponibile
+	let redImp = (letVarGuadagn * coefficiente) / 100;
+	console.log(redImp);
 
-// Calcolo reddito imponibile
-let redImp = (guadagnoAnnuale * coefficiente) / 100;
-console.log(redImp);
+	// Imposta da pagare
+	let imposta = (redImp * impSostitut) / 100;
+	console.log("Imposta sostitutiva al " + impSostitut + "%: " + imposta + "€");
 
-// Imposta da pagare
-let imposta = (redImp * impSostitut) / 100;
-console.log("Imposta sostitutiva al " + impSostitut + "%: " + imposta + "€");
-
-document.getElementById("guadagno-annuale").textContent = "Guadagno inserito: " + guadagnoAnnuale + "€";
-document.getElementById("imposta").textContent = "Imposta da pagare: " + imposta + "€";
+	document.getElementById("guadagno-annuale").textContent = "Guadagno inserito: " + letVarGuadagn + "€";
+	document.getElementById("imposta").textContent = "Imposta da pagare: " + imposta + "€";
+}
